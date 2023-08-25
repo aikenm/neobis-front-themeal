7bc230ba568865935ca47a5c5488d0e2f8c2523b
aikenm <a.mnsbkv@gmail.com>
2023-08-25 14:24:27 +0600

    Implemented correct positioning on the ingredients page

--- src/components/MealDetail.jsx
+++ src/components/MealDetail.jsx
@@ -6,25 +6,28 @@ const MealDetail = ({ meal }) => {
   }
 
   return (
-    <section className='meal-details'>
-    <div className='info-block'>
-      <h2 className='meal-name'>{meal.strMeal}</h2>
-      <p className='meal-category'>{meal.strCategory}</p>
-      <p className='meal-country'> | {meal.strArea}</p>
-      <h3 className='ingrediens-title'>Ingredients</h3>
-      </div>
-      <img src={meal.strMealThumb} alt={meal.strMeal} className='meal-image'/>
-      <ul>
-        {Object.entries(meal)
-          .filter(([key, value]) => key.startsWith('strIngredient') && value)
-          .map(([key, value], index) => (
-            <li key={index}>
-              {value} - {meal[`strMeasure${key.slice(13)}`]}
-            </li>
-          ))}
-      </ul>
+    <section className='meal-details-section'>
+    <div className='meal-details'>
+        <div className='md-info-block'>
+            <h2 className='md-meal-name'>{meal.strMeal}</h2>
+            <p className='md-meal-category'>{meal.strCategory}</p>
+            <p className='md-meal-country'> | {meal.strArea}</p>
+            <ul className='ingredients'>
+                {Object.entries(meal)
+                .filter(([key, value]) => key.startsWith('strIngredient') && value)
+                .map(([key, value], index) => (
+                    <li key={index} className='ingredient'>
+                    {value} - {meal[`strMeasure${key.slice(13)}`]}
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <img src={meal.strMealThumb} alt={meal.strMeal} className='md-meal-image'/>
+    </div>
+    <div className='instructions-block'>
       <h3 className='intstructions-title'>Instructions</h3>
       <p className='intstructions-para'>{meal.strInstructions}</p>
+    </div>
     </section>
   );
 };
