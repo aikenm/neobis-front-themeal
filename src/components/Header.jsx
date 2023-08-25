78e67c7b13e90d58d21d4fb006124d18ea6d9387
aikenm <a.mnsbkv@gmail.com>
2023-08-25 22:32:20 +0600

    Implemented the navigation to the main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/style.css';
 
 const Header = () => {
+
+const navigate = useNavigate();
+
   return (
     <header className='header'>
+        <Link to="/" onClick={() => navigate('/')} className='logo-link'>
       <h1 className='header-logo'>The Meal</h1>
+      </Link>
     </header>
   );
 };
